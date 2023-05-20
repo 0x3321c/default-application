12390ab3671d7dea8c19f2c62346335a909b172f
egiberne <29532199+egiberne@users.noreply.github.com>
2023-05-20 11:35:39 -0400

    Update Set-Default-Apps.ps1
    Entry for ProdId in the HKLM Hive

--- Set-Default-Apps.ps1
+++ Set-Default-Apps.ps1
@@ -1,25 +1,28 @@
 $extension = Read-Host "Enter the file extension (without the dot):"
 $application = Read-Host "Enter the path to the default application:"
 
-# Check if the provided application exists
-if (-not (Test-Path -Path $application)) {
-    Write-Host "The specified application path does not exist."
-    exit
-}
+if (-not (Test-Path -Path $application)) { Write-Host "The specified application path does not exist."; exit }
 
-# Define the default application for the extension
-$progId = "CustomProgId.$extension"
+$customProgId = "CustomProgId.$extension"
+$regPath = "HKCU:\Software\Classes\$customProgId"
+$regPathHKLM = "HKLM:\Software\Classes\$customProgId"
 
-# Create a new ProgId key for the extension
-$regPath = "HKCU:\Software\Classes\$progId"
+# Create ProgId registry keys
 New-Item -Path $regPath -Force | Out-Null
+New-Item -Path $regPathHKLM -Force | Out-Null
 
-# Set the default value of the ProgId key to the extension
+# Set the default value of the ProgId keys to the extension
 Set-ItemProperty -Path $regPath -Name "(Default)" -Value $extension
+Set-ItemProperty -Path $regPathHKLM -Name "(Default)" -Value $extension
 
 # Create the Open command key and set the application as the default handler
 $openCommandPath = "$regPath\shell\open\command"
+$openCommandPathHKLM = "$regPathHKLM\shell\open\command"
+
 New-Item -Path $openCommandPath -Force | Out-Null
+New-Item -Path $openCommandPathHKLM -Force | Out-Null
+
 Set-ItemProperty -Path $openCommandPath -Name "(Default)" -Value "`"$application`" `"%1`""
+Set-ItemProperty -Path $openCommandPathHKLM -Name "(Default)" -Value "`"$application`" `"%1`""
 
 Write-Host "Default application set for '.$extension' files."
