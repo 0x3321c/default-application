a2177f3df759639eedabe996cf858f44f0a6d6f3
egiberne <29532199+egiberne@users.noreply.github.com>
2023-05-21 00:01:24 -0400

    Update Set-Default-Apps.ps1
    Working version tested as following : 
    Enter the file extension (without the dot):: text
    Enter the path to the default application:: c:\windows\notepad.exe
    Default application set for '.text' files.

--- Set-Default-Apps.ps1
+++ Set-Default-Apps.ps1
@@ -1,19 +1,44 @@
 $extension = Read-Host "Enter the file extension (without the dot):"
-$application = Read-Host "Enter the path to the default application:"
+$apppath = Read-Host "Enter the path to the default application:"
+$EXT = $extension.ToUpper()
 
 if (-not (Test-Path -Path $application)) { Write-Host "The specified application path does not exist."; exit }
 
-$customProgId = "CustomProgId.$extension"
+$executable=$apppath.Split("\")[-1]
+$app=$executable.Split(".")[0]
+
+$customProgId = "$app.$extension"
 $regPath = "HKCU:\Software\Classes\$customProgId"
 $regPathHKLM = "HKLM:\Software\Classes\$customProgId"
 
+$regPathEXT = "HKCU:\Software\Classes\.$extension"
+$regPathEXTHKLM = "HKLM:\Software\Classes\.$extension"
+
 # Create ProgId registry keys
-New-Item -Path $regPath -Force | Out-Null
+New-Item -Path $regPathEXT -Force | Out-Null
 New-Item -Path $regPathHKLM -Force | Out-Null
 
+# Create Extension registry keys
+New-Item -Path $regPath -Force | Out-Null
+New-Item -Path $regPathEXTHKLM -Force | Out-Null
+
 # Set the default value of the ProgId keys to the extension
-Set-ItemProperty -Path $regPath -Name "(Default)" -Value $extension
-Set-ItemProperty -Path $regPathHKLM -Name "(Default)" -Value $extension
+Set-ItemProperty -Path $regPath -Name "(Default)" -Value "$EXT Source File" 
+Set-ItemProperty -Path $regPathHKLM -Name "(Default)" -Value "$EXT Source File" 
+
+# Set OpenWithProgids for the extension
+$openWithProgids = @{
+    $customProgId = ''
+}
+
+New-Item -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\FileExts\.$extension\OpenWithProgids" -Force | Out-Null
+New-Item -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\FileExts\.$extension\OpenWithList" -Force | Out-Null
+
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\FileExts\.$extension\OpenWithList" -Name "a" -Value $executable
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\FileExts\.$extension\OpenWithList" -Name "MRUList" -Value "a"
+
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\FileExts\.$extension\OpenWithProgids" -Name $extension -Value ""
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\FileExts\.$extension\OpenWithProgids" -Name $customProgId -Value ""
 
 # Create the Open command key and set the application as the default handler
 $openCommandPath = "$regPath\shell\open\command"
